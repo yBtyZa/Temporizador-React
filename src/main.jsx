ee5266567e1d56503711ca2558028a1a28be0024
Guilherme Betsa <ybtyza@gmail.com>
2024-08-29 16:02:44 -0300

    feat: 🎸 (Autenticação) Rotas login e HomePage com autenticação

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routers'
+import { AuthProvider } from './context/AuthContext'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
 )
