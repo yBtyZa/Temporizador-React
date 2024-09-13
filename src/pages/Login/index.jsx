aea622a838fce14fa4b34bf1bc90026482ca42d2
Guilherme Betsa <ybtyza@gmail.com>
2024-09-13 17:19:56 -0300

    feat: 🎸 (refactor) Refatorando o código

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,34 +1,10 @@
+import LoginT from "../../components/templates/LoginT";
 import styles from "./styles.module.css";
-import { useAuth } from '../../context/AuthContext'
-import { useForm } from 'react-hook-form'
 
 function Login() {
-    const { login } = useAuth();
-    const { register, handleSubmit } = useForm();
-
-    const onSubmit = async (userData) => {
-        await login(userData);
-    }
-
   return (
     <div className={styles.login}>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email", {
-            required: true
-        })}/>
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" {...register("password", {
-            required: true,
-            minLength: 6
-        })}/>
-        <button
-          type="submit"
-        >
-          Login
-        </button>
-      </form>
+      <LoginT />
     </div>
   );
 }
